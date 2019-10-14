d00724772548691cf203ef3c55caccd2d1cea3a0
Ani Zivzivadze <anizivzivadze86@gmail.com>
2019-10-14 21:19:47 +0300

    index page configuration

--- Folders/PropertyManagement/PropertyManagement/Controllers/HomeController.cs
+++ Folders/PropertyManagement/PropertyManagement/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace PropertyManagement.Controllers
     {
         public ActionResult Index(string search = null)
         {
-            var thumbnails1 = new List<ThumbnailModel>().GetHouse1Thumbnail(ApplicationDbContext.Create(), search);
+            /*var thumbnails1 = new List<ThumbnailModel>().GetHouse1Thumbnail(ApplicationDbContext.Create(), search);
             var thumbnails = new List<ThumbnailModel>().GetHouseThumbnail(ApplicationDbContext.Create(), search);
             var count = thumbnails.Count() / 4;
             
@@ -31,9 +31,52 @@ namespace PropertyManagement.Controllers
 
 
             return View(model);
+            */
+            return View();
+        }
+        public ActionResult ForSale(string search = null)
+        {
+
+            var thumbnails1 = new List<ThumbnailModel>().GetHouse1Thumbnail(ApplicationDbContext.Create(), search);
+            var thumbnails = new List<ThumbnailModel>().GetHouseThumbnail(ApplicationDbContext.Create(), search);
+            var count = thumbnails.Count() / 4;
+
+            var model = new List<ThumbnailBoxViewModel>();
+
+            for (int i = 0; i <= count; i++)
+            {
+                model.Add(new ThumbnailBoxViewModel
+                {
+                    Thumbnails1 = thumbnails1.Skip(i * 4).Take(4),
+                    Thumbnails = thumbnails.Skip(i * 4).Take(4)
+                });
+            }
+
+
+            return View(model);
 
         }
+        public ActionResult ForRent(string search = null)
+        {
 
+            var thumbnails1 = new List<ThumbnailModel>().GetHouse1Thumbnail(ApplicationDbContext.Create(), search);
+            var thumbnails = new List<ThumbnailModel>().GetHouseThumbnail(ApplicationDbContext.Create(), search);
+            var count = thumbnails.Count() / 4;
+
+            var model = new List<ThumbnailBoxViewModel>();
+
+            for (int i = 0; i <= count; i++)
+            {
+                model.Add(new ThumbnailBoxViewModel
+                {
+                    Thumbnails1 = thumbnails1.Skip(i * 4).Take(4),
+                    Thumbnails = thumbnails.Skip(i * 4).Take(4)
+                });
+            }
+
+
+            return View(model);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
